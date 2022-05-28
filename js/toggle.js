d53039c08377edcb64c3584a45bc9a85fcfbdf5c
Hama DMC <ecorp87@gmail.com>
2022-05-28 13:46:33 +0300

    first commit

---
+++ js/toggle.js
@@ -0,0 +1,10 @@
+const targetDiv = document.getElementById("togglethis");
+const btn = document.getElementById("toggle");
+btn.onclick = function () {
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+  } else {
+    targetDiv.style.display = "block";
+
+  }
+};
